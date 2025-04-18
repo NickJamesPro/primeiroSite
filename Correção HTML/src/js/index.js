933926a8725efb8f3ceba104572b100b53136474
Daniel Programações <danielzing17@gmail.com>
2025-04-18 18:07:47 -0300

    Add files via upload

---
+++ Correção HTML/src/js/index.js
@@ -0,0 +1,18 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+//passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        //passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+
+        botao.classList.add("selecionado");
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+
+        personagens[indice].classList.add("selecionado");
+    });
+});
